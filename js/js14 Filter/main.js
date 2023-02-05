9401a366ec125dacf1c66b3812b05c68fa9ee933
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-02-05 23:09:33 +0100

    refaktoring

--- js/js14 Filter/main.js
+++ js/js14 Filter/main.js
@@ -1,31 +1,23 @@
 const users = [
-    { id: 0, name: "ahmet", age: 25 },
-    { id: 1, name: "mehmet", age: 15 },
-    { id: 3, name: "dervis", age: 35 },
-    { id: 4, name: "adervis", age: 25 },
-  ];
-  
-  const kirktanKucukler = [];
-  
-  const test = users.map((asd, index, array) => {
-    if (asd.age < 30) {
-      return asd;
-    } else {
-      return "bigger than 30";
-    }
-  
-    //return
-  });
-  
-  console.log(test);
-  
-  // users.forEach((asd, index, array) => {
-  //   if (asd.age < 30) {
-  //     kirktanKucukler.push(asd.name);
-  //   } else {
-  //     console.log(asd.name);
-  //   }
-  // });
-  
-  console.log(kirktanKucukler);
-  
+  { id: 0, name: "ahmet", age: 25 },
+  { id: 1, name: "mehmet", age: 15 },
+  { id: 3, name: "dervis", age: 35 },
+];
+
+const kirktanKucukler = [];
+
+const test = users.filter((asd, index, array) => {
+  if (asd.age < 30) {
+    return true;
+  }
+});
+
+console.log(test);
+
+// users.forEach((asd, index, array) => {
+//   if (asd.age < 30) {
+//     kirktanKucukler.push(asd.name);
+//   } else {
+//     console.log(asd.name);
+//   }
+// });
