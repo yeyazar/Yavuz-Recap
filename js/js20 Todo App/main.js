07371b0910546e40f1aa5a496f2536d9a243369b
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-02-06 22:32:38 +0100

    todo

--- js/js20 Todo App/main.js
+++ js/js20 Todo App/main.js
@@ -3,24 +3,32 @@
 // yeni bit todo olustur
 // ve bunu DOM un icine koy
 
-{
-  /* <li class="todoItem">
-<button class="completeButton">Complete</button>
-<button class="deleteButton">Delete</button>
-<p class="todoText">Test something</p>
-</li>
-<li class="todoItem completed">
-<button class="completeButton">Complete</button>
-<button class="deleteButton">Delete</button>
-<p class="todoText">Test something</p>
-</li> */
-}
-
 const addTaskButton = document.querySelector(".addTask");
 const todoInput = document.querySelector(".todoInput");
 const todosContainer = document.querySelector(".todos");
 
 console.log(addTaskButton, todoInput);
+
+const toggleComplete = (event) => {
+  const { parentElement } = event.currentTarget;
+
+  const isCompleted = [...parentElement.classList].some(
+    (className) => className === "completed"
+  );
+
+  if (!isCompleted) {
+    event.currentTarget.innerText = "Todo";
+  } else {
+    event.currentTarget.innerText = "Completed";
+  }
+
+  event.currentTarget.parentElement.classList.toggle("completed");
+};
+
+const removeItem = (event) => {
+  todosContainer.removeChild(event.currentTarget.parentElement)
+  //console.log(event.currentTarget.parentElement);
+};
 
 const renderTodoItem = (todoText) => {
   const todoItemElement = document.createElement("li");
@@ -30,15 +38,18 @@ const renderTodoItem = (todoText) => {
   const completeButton = document.createElement("button");
   completeButton.classList.add("completeButton");
   completeButton.innerText = "Complete";
+  completeButton.addEventListener("click", toggleComplete);
   todoItemElement.appendChild(completeButton);
 
   const deleteButton = document.createElement("button");
   deleteButton.classList.add("deleteButton");
   deleteButton.innerText = "Delete";
+  deleteButton.addEventListener("click", removeItem);
   todoItemElement.appendChild(deleteButton);
 
   const textElement = document.createElement("p");
   textElement.innerText = todoText;
+  textElement.classList.add("todoText");
   todoItemElement.appendChild(textElement);
 
   todosContainer.appendChild(todoItemElement);
