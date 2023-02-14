e3d12035a34c93ec1a406e06a71ce0b5ca5f81a5
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-02-14 19:54:11 +0100

    css

--- react/classes/rc02_jsx-comp-sytling/src/App.js
+++ react/classes/rc02_jsx-comp-sytling/src/App.js
@@ -1,3 +1,4 @@
+import Content from "./Content";
 import { Footer } from "./Footer";
 import Header from "./Header";
 
@@ -7,6 +8,7 @@ function App() {
     //* jsx
     <>
       <Header />
+      <Content />
       <Footer />
     </>
   );
