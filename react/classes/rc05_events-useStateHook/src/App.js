a54144d4f7404ce0e22db179f2f42e7c39011548
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-02-18 12:20:10 +0100

    objects

--- react/classes/rc05_events-useStateHook/src/App.js
+++ react/classes/rc05_events-useStateHook/src/App.js
@@ -1,11 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Event from "./components/1-events/Event"
+import Event from "./components/1-events/Event";
+import Counter from "./components/2-classComponent/Counter";
+import UseStateCounter from "./components/3-useStateHook/UseStateCounter";
+import UseStateObject from "./components/3-useStateHook/UseStateObject";
 
 function App() {
   return (
     <div>
       <Event />
+      <Counter />
+      <UseStateCounter />
+      <UseStateObject />
     </div>
-  )
+  );
 }
-export default App
+export default App;
--- react/classes/rc05_events-useStateHook/src/components/2-classComponent/Counter.jsx
+++ react/classes/rc05_events-useStateHook/src/components/2-classComponent/Counter.jsx
@@ -11,8 +11,56 @@
 import React from "react"
 
 class Counter extends React.Component {
+  constructor(props) {
+    super(props)
+
+    //!  State'e baslangi degeri verdik.
+    this.state = {
+      count: 0,
+    }
+
+    //  this.increment = function (){
+    //  }
+
+    //? increment metodunun Counter class'ina baglanmasi (bind)
+    //  this.increment = this.increment.bind(this)
+  }
+
+  //! Yazmis oldugumuz metotlar default olarak classa baglanmaz.
+  //! Ancak, React built-in fonksiyonlari baglidir.
+
+  //* Bunun icin metotlarimizi ya constructor icerisinde baglamaliyiz yada otomatik baglamayi saglayan arrow fonksiyonlarini kullanmaliyiz.
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    })
+  }
   render() {
-    return <div>Counter</div>
+    return (
+      <div className="container text-center mt-4">
+        <h2 className="text-danger">Class Components</h2>
+        <h1 className="display-4">Count:{this.state.count}</h1>
+        <button onClick={this.increment} className="btn btn-success">
+          INC
+        </button>
+        <button
+          onClick={() => this.setState({ count: 0 })}
+          className="btn btn-danger"
+        >
+          CLR
+        </button>
+        <button onClick={this.decrement} className="btn btn-warning">
+          DEC
+        </button>
+      </div>
+    )
   }
 }
 
