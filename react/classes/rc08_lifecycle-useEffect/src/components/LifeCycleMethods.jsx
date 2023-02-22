f799cf26463f0eb7aafcce9c2ff0dd6b4ab59cfc
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-02-22 20:58:35 +0100

    user.jsx

--- react/classes/rc08_lifecycle-useEffect/src/components/LifeCycleMethods.jsx
+++ react/classes/rc08_lifecycle-useEffect/src/components/LifeCycleMethods.jsx
@@ -19,8 +19,9 @@
 import React from "react"
 
 class LifeCycleMethods extends React.Component {
+  //! 1-) Bir componentin olsuturulmasinda cagrilir
   constructor(props) {
-    console.log("1- constructor");
+    console.log("1- Constructor running")
     super(props)
     this.state = {
       count: 0,
@@ -33,16 +34,30 @@ class LifeCycleMethods extends React.Component {
     })
   }
 
-  componentDidMount(){
-    console.log("3- component Did Mount");
+  //! 3-) Bir component DOM agacina eklendiginde calistirilir.
+  //! (İlk render sonrasi).
+  //! Her yasam dongusu icin bir kere calisir.
+  componentDidMount() {
+    console.log("3-componentdidmount")
   }
 
-  componentDidUpdate(){
-    console.log("comp Updated");
+  //! 4-) Bu metot ilk render haric diger tüm render'lardan sonra cagrilir.
+  //!  prevState ve prevProps degerlerini parametre olarak alabilir.
+  componentDidUpdate(prevState) {
+    console.log("4-componentDidUpdate")
+    // if(prevState.dolarParite - this.dolarParite > 3){
+    //   mailgönder()
+    // }
+  }
+
+  //! 5-) Bir component DOM agacindan kaldiriltiktan hemen sonra cagirlir.
+  componentWillUnmount() {
+    console.log("5-componentWillUnmount")
   }
 
   render() {
-    console.log("2- render");
+    //! 2-) Her bir state yada prop degistiginde (render) cagrilir
+    console.log("2-Rendered")
     return (
       <div className="container text-center">
         <h1 className="text-danger">LIFECYCLE METHODS</h1>
