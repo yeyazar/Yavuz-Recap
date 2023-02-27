93a819394793d8ebd3da10b36ec9ecf33d7458de
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-02-27 20:02:57 +0100

    onChangedatepname

--- react/classes/rc10_appoinment-project/src/components/AddModal.jsx
+++ react/classes/rc10_appoinment-project/src/components/AddModal.jsx
@@ -1,28 +1,43 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
+import { useState } from "react";
 
 export function AddModal({ show, handleClose, drName }) {
+  const [patientName, setPatientName] = useState("");
+  const [date, setDate] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // ------------
+    handleClose();
+  };
+
+  console.log(patientName, date);
+
   return (
     <>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title className="text-danger">Appointment for {drName}</Modal.Title>
+          <Modal.Title className="text-danger">
+            Appointment for {drName}
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="name">
               <Form.Label>Patient Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter your name" />
+              <Form.Control type="text" placeholder="Enter your name" onChange={(e)=>setPatientName(e.target.value)}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="datetime">
               <Form.Label>Day&Time</Form.Label>
-              <Form.Control type="datetime-local" />
+              <Form.Control type="datetime-local" onChange={(e)=>setDate(e.target.value)}/>
             </Form.Group>
 
             <div className="text-center">
-              <Button variant="primary" className="me-2" type="submit" onClick={handleClose}>
+              <Button variant="primary" className="me-2" type="submit">
                 Save
               </Button>
               <Button variant="danger" onClick={handleClose}>
