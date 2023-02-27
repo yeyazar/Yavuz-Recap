a5e66c044d240970d76193343e891cefd49175a2
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-02-27 18:59:46 +0100

    addmodal

--- react/classes/rc10_appoinment-project/src/components/Doctors.jsx
+++ react/classes/rc10_appoinment-project/src/components/Doctors.jsx
@@ -5,16 +5,20 @@ import Col from "react-bootstrap/Col";
 const Doctors = ({ doctors }) => {
   return (
     <Container className="p-2">
-      <h3 className="display-6" style={{ color: "rgb(166, 18, 189)" }}>
+      <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
         Our Doctors
       </h3>
 
-      <Row>
+      <Row className="justify-content-center">
         {doctors.map((dr) => (
-          <Col key={dr.id} sm={6} md={4} lg={3}>
-            <img src={dr.img} alt={dr.name} />
-            <h4>{dr.name}</h4>
-            <h5>{dr.dep}</h5>
+          <Col key={dr.id} xs={6} sm={4} md={3} lg={3}>
+            <img
+              src={dr.img}
+              alt={dr.name}
+              className="img-thumbnail doctor-img"
+            />
+            <h5>{dr.name}</h5>
+            <h6>{dr.dep}</h6>
           </Col>
         ))}
       </Row>
