a81f0f36f997f5f3ae37a1263abd1b7cd3503fda
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-02-27 19:28:01 +0100

    setShow

--- react/classes/rc10_appoinment-project/src/components/AddModal.jsx
+++ react/classes/rc10_appoinment-project/src/components/AddModal.jsx
@@ -2,18 +2,11 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
-export function AddModal() {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+export function AddModal({show, handleClose}) {
+ 
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
@@ -31,5 +24,3 @@ export function AddModal() {
     </>
   );
 }
-
-export default AddModal;
--- react/classes/rc10_appoinment-project/src/components/Doctors.jsx
+++ react/classes/rc10_appoinment-project/src/components/Doctors.jsx
@@ -1,8 +1,15 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { AddModal } from "./AddModal";
+import { useState } from "react";
 
 const Doctors = ({ doctors }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
@@ -16,12 +23,14 @@ const Doctors = ({ doctors }) => {
               src={dr.img}
               alt={dr.name}
               className="img-thumbnail doctor-img"
+              onClick={handleShow}
             />
             <h5>{dr.name}</h5>
             <h6>{dr.dep}</h6>
           </Col>
         ))}
       </Row>
+      <AddModal show={show} handleClose={handleClose} />
     </Container>
   );
 };
