3bc027e405d769d5eaa036a8fd11dea51858b552
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-02-28 19:27:50 +0100

    browserrouter

--- react/classes/rc11_router-intro/router-intro-inClass/src/App.js
+++ react/classes/rc11_router-intro/router-intro-inClass/src/App.js
@@ -1,21 +1,26 @@
-import Footer from "./components/Footer"
-import Nav from "./components/Nav"
-import Contact from "./pages/Contact"
-import Home from "./pages/Home"
-import Instructors from "./pages/Instructors"
-import NotFound from "./pages/NotFound"
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Instructors from "./pages/Instructors";
+import NotFound from "./pages/NotFound";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Nav />
-      <Home />
-      <Instructors />
-      <Contact />
-      <NotFound />
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/instructors" element={<Instructors />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
       <Footer />
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
