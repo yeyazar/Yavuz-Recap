8b08d84cb8423a883add14a3b21f1d24550777bb
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-03-01 20:55:58 +0100

    login

--- react/classes/rc11-12_router-example/rc11-12_router-example-inClass/src/App.js
+++ react/classes/rc11-12_router-example/rc11-12_router-example-inClass/src/App.js
@@ -10,6 +10,7 @@ import Paths from "./pages/Paths";
 import FullStack from "./pages/Fullstack";
 import PrivateRouter from "./pages/PrivateRouter";
 import Aws from "./pages/Aws";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -21,14 +22,15 @@ function App() {
         <Route path="/people" element={<People />} />
         <Route path="/people/:id" element={<PersonDetail />} />
 
-        <Route path="/contact" element={<PrivateRouter/>}>
-          <Route path="" element={<Contact />} />
-        </Route>
-
         <Route path="/paths" element={<Paths />}>
           <Route path="" element={<FullStack />} />
           <Route path="aws" element={<Aws />} />
         </Route>
+
+        <Route path="/contact" element={<PrivateRouter />}>
+          <Route path="" element={<Contact />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
--- react/classes/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PrivateRouter.jsx
+++ react/classes/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PrivateRouter.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Navigate, Outlet } from "react-router-dom";
+
+//? In practice this part is contained in Global State's. (Context API, Redux, Mobx)
+//! const isSigned = userContext(jwt)
+const isSigned = true;
 
 const PrivateRouter = () => {
-  return (
-    <div>PrivateRouter</div>
-  )
-}
 
-export default PrivateRouter
+  return (
+    <div>
+      {isSigned ? <Outlet /> : <Navigate to="/login" />}
+    </div>
+  );
+};
+
+export default PrivateRouter;
