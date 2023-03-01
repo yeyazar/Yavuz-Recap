a2b047b37521e02ff0b731c21b8a3cecd0ad8e38
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-03-01 19:42:02 +0100

    paths

--- react/classes/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/Paths.jsx
+++ react/classes/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/Paths.jsx
@@ -1,7 +1,32 @@
-const Paths = () => {
-  return (
-    <div>Paths</div>
-  )
-}
+import { Link, useNavigate, Outlet } from "react-router-dom";
 
-export default Paths
+const Paths = () => {
+  
+  return (
+    <div className="container mt-4">
+      <h1>
+        Online IT Courses to Become a Qualified IT Professional with Clarusway
+      </h1>
+
+      <p className="fs-5">
+        Join outstanding companies with rewarding salaries. We offer the
+        highest-demand IT skills YOU need for success! CHOOSE THE BEST COURSE
+        FOR YOU Upgrade your career with the best online training led by top IT
+        experts!
+      </p>
+      <div>
+        <Link className="btn btn-success w-50" to="">
+          Fullstack
+        </Link>
+        <button
+          className="btn btn-warning w-50"
+        >
+          Aws-Devops
+        </button>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Paths;
