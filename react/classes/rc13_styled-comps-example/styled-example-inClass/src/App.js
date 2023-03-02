6c5c9fbdf6883b625091abede2f8d58f22c31a07
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-03-02 21:25:11 +0100

    themeprovider

--- react/classes/rc13_styled-comps-example/styled-example-inClass/src/App.js
+++ react/classes/rc13_styled-comps-example/styled-example-inClass/src/App.js
@@ -2,14 +2,30 @@ import Header from "./components/Header";
 import Container from "./components/styled/Container";
 import { GlobalStyles } from "./components/styled/GlobalStyles";
 
+import { ThemeProvider } from "styled-components";
+
+const styles = {
+  colors: {
+    primary: "#444",
+    secondary: "#666",
+  },
+  margins: {},
+  paddings: {},
+  breakpoints: {
+    xs: "300px",
+    sm: "500px",
+    md: "700px",
+  },
+};
+
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={styles}>
       <GlobalStyles />
       <Container>
         <Header />
       </Container>
-    </>
+    </ThemeProvider>
   );
 };
 
