0a9556e5af33df998eca5d347f1743bf564a65e8
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-03-02 19:03:46 +0100

    exports

--- react/classes/rc13_styled-comp-intro/styled-intro_inClass/src/App.js
+++ react/classes/rc13_styled-comp-intro/styled-intro_inClass/src/App.js
@@ -1,7 +1,14 @@
+import { Button } from "./components/Buton.styled";
+import { FS13Button } from "./components/Buton.styled";
+
 const App = () => {
   return (
     <>
       <h1>Styled Components</h1>
+      <Button>Send</Button>
+      <Button primary>Send</Button>
+      <FS13Button react>Work</FS13Button>
+      <FS13Button >Work fckin More!</FS13Button>
     </>
   );
 };
