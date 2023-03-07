da88925c261e28cb0236f40c56d8b3e913219663
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-03-07 20:38:44 +0100

    navbar

--- react/classes/rc14-15_clarus-library/clarus-library-inclass/src/components/navbar/Navbar.jsx
+++ react/classes/rc14-15_clarus-library/clarus-library-inclass/src/components/navbar/Navbar.jsx
@@ -1,18 +1,20 @@
-import React from "react";
+import { useState } from "react";
 import { menuIcon } from "../../helper/iconData";
 import { HamburgerIcon, Logo, Menu, MenuLink, Nav } from "./Navbar.style";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false)
+
   return (
-    <Nav justify="space-between">
+    <Nav justify="space-between" wrap="wrap">
       <Logo to="/">Navy's Library</Logo>
-      <HamburgerIcon>{menuIcon}</HamburgerIcon>
-      <Menu showMenu={true}>
+      <HamburgerIcon onClick={()=>setShowMenu(!showMenu)}>{menuIcon}</HamburgerIcon>
+      <Menu showMenu={showMenu}>
         <MenuLink to="/">Home</MenuLink>
         <MenuLink to="/about">About</MenuLink>
         <MenuLink to="/register">Register</MenuLink>
         <MenuLink to="/login">Login</MenuLink>
-        <MenuLink to="/">logout</MenuLink>
+        <MenuLink to="/login">logout</MenuLink>
       </Menu>
     </Nav>
   );
