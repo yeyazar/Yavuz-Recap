9440717b33a27b11418d408c96830105d7b6f931
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-03-08 18:26:48 +0100

    home

--- react/classes/rc14-15_clarus-library/clarus-library-inclass/src/pages/home/Home.jsx
+++ react/classes/rc14-15_clarus-library/clarus-library-inclass/src/pages/home/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Header from '../../components/header/Header'
+import { HomeContainer, HomeImage, MainContainer } from './Home.style'
+import homeImage from "../../assets/books.jpg"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <HomeContainer>
+      <Header/>
+
+      <HomeImage>
+        <img src={homeImage} alt="home-image"/>
+      </HomeImage>
+
+      <MainContainer>
+        
+      </MainContainer>
+
+    </HomeContainer>
   )
 }
 
