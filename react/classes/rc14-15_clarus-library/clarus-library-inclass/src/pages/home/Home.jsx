ca8d54b96846af903f31c6d8e514f804c8fd35fc
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-03-08 19:41:26 +0100

    home

--- react/classes/rc14-15_clarus-library/clarus-library-inclass/src/pages/home/Home.jsx
+++ react/classes/rc14-15_clarus-library/clarus-library-inclass/src/pages/home/Home.jsx
@@ -3,36 +3,47 @@ import Header from "../../components/header/Header";
 import { HomeContainer, HomeImage, MainContainer } from "./Home.style";
 import homeImage from "../../assets/books.jpg";
 import axios from "axios";
+import Card from "../../components/card/Card";
 
 const Home = () => {
   // const printType = ["all", "books", "magazines"];
   const [query, setQuery] = useState("");
   const [selectType, setSelectType] = useState("all");
-  const [myData, setMyData] = useState(null);
+  const [myData, setMyData] = useState([]);
   const APP_KEY = process.env.REACT_APP_apiKey;
 
   const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&printType=${selectType}&key=${APP_KEY}`;
 
   const getData = async () => {
-
     try {
-      const {data} = await axios.get(url)
-      console.log(data);
+      const { data } = await axios.get(url);
+      // console.log(data.items);
+      // data.items.map((book) => console.log(book.volumeInfo.title));
+      setMyData(data.items);
     } catch (error) {
       console.log(error);
     }
-
-  }
+  };
 
   return (
     <HomeContainer>
-      <Header setQuery={setQuery} setSelectType={setSelectType} getData={getData}/>
+      <Header
+        setQuery={setQuery}
+        setSelectType={setSelectType}
+        getData={getData}
+      />
 
-      <HomeImage>
-        <img src={homeImage} alt="home-image" />
-      </HomeImage>
-
-      <MainContainer></MainContainer>
+      {!myData.length ? (
+        <HomeImage>
+          <img src={homeImage} alt="home-image" />
+        </HomeImage>
+      ) : (
+        <MainContainer wrap="wrap">
+          {
+            myData.map((item)=> <Card key={item.id} item={item}/>)
+          }
+        </MainContainer>
+      )}
     </HomeContainer>
   );
 };
